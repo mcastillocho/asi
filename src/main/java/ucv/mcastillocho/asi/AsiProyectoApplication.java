cf0ad121b7432437fc3d622b64824706b67c0d5e
Madgen Del Castillo <madgen.delcastillo@gmail.com>
2025-10-18 09:47:13 -0500

    Inicializacion Proyecto

---
+++ src/main/java/ucv/mcastillocho/asi/AsiProyectoApplication.java
@@ -0,0 +1,13 @@
+package ucv.mcastillocho.asi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsiProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsiProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ucv/mcastillocho/asi/AsiProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package ucv.mcastillocho.asi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsiProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
